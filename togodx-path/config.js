9ff08683e35d9a2e249ab5de67b66111007ec4d6
Hirokazu Chiba <hchiba1@gmail.com>
2022-01-21 15:25:11 +0900

    Update config.js

--- togodx-path/config.js
+++ togodx-path/config.js
@@ -1,4 +1,3 @@
-
 {
   node: {
     caption: ['id'],
@@ -8,25 +7,4 @@
     caption: ['label'],
   },
   layout: 'default',
-
-  /*
-  layout: 'hierarchical',
-  layoutSettings: {
-    enabled:true,
-    levelSeparation: 150,
-    nodeSpacing: 100,
-    treeSpacing: 200,
-    blockShifting: true,
-    edgeMinimization: true,
-    parentCentralization: true,
-    direction: 'UD',        // UD, DU, LR, RL
-    sortMethod: 'hubsize',  // hubsize, directed
-    shakeTowards: 'leaves'  // roots, leaves
-  },
-  layout: 'custom',
-  layoutSettings: {
-    x: 'x',
-    y: 'y'
-  },
-  */
 }
