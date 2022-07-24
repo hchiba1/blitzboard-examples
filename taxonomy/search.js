f46e855a08ef183c81b234490c969125e531ce3a
Hirokazu Chiba <hchiba1@gmail.com>
2022-07-24 15:26:03 +0900

    Add addNode() getThumb() and use in taxonomy/search.js

--- taxonomy/search.js
+++ taxonomy/search.js
@@ -35,14 +35,11 @@ $(function () {
       if (ui.item) {
         const name = ui.item.label;
         sparqlTaxonomy(name, (json) => {
-          const url = json.results.bindings[0].url.value;
-          const rank = json.results.bindings[0].rank.value;
-          const m = url.match(/.*\/(\d+)/);
-          if (m) {
-            const taxid = m[1];
-            let pg = `${taxid} :Taxon "taxon name": "${name}" "name": "${name}"`;
-            blitzboard.setGraph(pg, true);
-          }
+          blitzboard.setGraph('', true);
+          addNode(name, json.results.bindings[0], (id) => {
+            blitzboard.update();
+            blitzboard.network.fit();
+          });
         });
       }
     }
@@ -62,5 +59,70 @@ function sparqlTaxonomy(name, callback) {
     return res.json();
   }).then(json => {
     callback(json);
+  });
+}
+
+function getThumb(name, callback) {
+  const sparqlGetThum = `
+        PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+        SELECT ?thumb ?name_ja ?rank_ja ?url ?descr_ja
+        WHERE {
+          ?url wdt:P225 "${name}" .
+          ?url rdfs:label ?name_ja .
+          ?url wdt:P105/rdfs:label ?rank_ja .
+          OPTIONAL {
+            ?url wdt:P18 ?thumb .
+          }
+          FILTER(lang(?name_ja) = 'ja')
+          FILTER(lang(?rank_ja) = 'ja')
+          OPTIONAL {
+            ?url <http://schema.org/description> ?descr_ja .
+            FILTER(lang(?descr_ja) = 'ja')
+          }
+        }`;
+  fetch(`https://query.wikidata.org/sparql?query=${encodeURIComponent(sparqlGetThum)}&format=json`).then(res => {
+    return res.json();
+  }).then(result => {
+    callback(result);
+  });
+}
+
+function addNode (name, elem, callback) {
+  let id = elem.url.value.replace(/.*\//g, '');
+  if (blitzboard.hasNode(id)) {
+    return;
+  }
+  let node = {
+    id: id,
+    labels: ['Taxon'],
+    properties: {
+      'taxon name': [name],
+      'taxon rank': [elem.rank.value],
+      'tax ID': [elem.url.value],
+    }
+  };
+  getThumb(name, (result) => {
+    for (let elem of result.results.bindings) {
+      if (elem.thumb?.value) {
+        node.properties.thumbnail = [elem.thumb.value];
+      }
+      if (elem.url?.value) {
+        node.properties.Wikidata = [elem.url.value];
+      }
+      if (elem.descr_ja?.value) {
+        node.properties.description = [elem.descr_ja.value];
+      }
+      if (elem.rank_ja?.value) {
+        node.properties.rank_ja = [elem.rank_ja.value];
+      }
+      if (elem.name_ja?.value) {
+        node.properties.name = [elem.name_ja.value];
+      }
+    }
+    if (!node.properties.name) {
+      node.properties.name = [elem.name.value];
+    }
+    blitzboard.addNode(node, true);
+    callback(id);
   });
 }
